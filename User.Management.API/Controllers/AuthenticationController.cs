c6083cc9d14a3a9709be69ac63f27bf658a75bc9
Abdulmumin-Rashed <abdulmuminr326@gmail.com>
2024-07-12 17:21:42 +0300

    Add project files.

---
+++ User.Management.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using User.Management.API.Models;
+using User.Management.Data.Models;
+using User.Management.Service.Models;
+using User.Management.Service.Models.Authentication.Login;
+using User.Management.Service.Models.Authentication.SignUp;
+using User.Management.Service.Models.Authentication.User;
+using User.Management.Service.Services;
+using static Humanizer.In;
+using static System.Net.WebRequestMethods;
+
+namespace User.Management.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IEmailService _emailService;
+        private readonly IUserManagement _user;
+
+        public AuthenticationController(UserManager<ApplicationUser> userManager,
+            IEmailService emailService,
+            IUserManagement user,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _emailService = emailService;
+            _user= user;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterUser registerUser)
+        {
+            var tokenResponse = await _user.CreateUserWithTokenAsync(registerUser);
+            if (tokenResponse.IsSuccess && tokenResponse.Response!=null)
+            {
+                await _user.AssignRoleToUserAsync(registerUser.Roles,tokenResponse.Response.User);
+
+               // var confirmationLink = $"http://localhost:4200/confirm-account?Token={tokenResponse.Response.Token}&email={registerUser.Email}";
+                var confirmationLink = Url.Action(nameof(ConfirmEmail), "Authentication", new { tokenResponse.Response.Token, email = registerUser.Email }, Request.Scheme);
+                
+                var message = new Message(new string[] { registerUser.Email! }, "Confirmation email link", confirmationLink!);
+                var responseMsg= _emailService.SendEmail(message);
+                return StatusCode(StatusCodes.Status200OK,
+                        new Response { IsSuccess=true, Message = $"{tokenResponse.Message} {responseMsg}" });
+
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                  new Response { Message = tokenResponse.Message,IsSuccess=false });
+        }
+
+        [HttpGet("ConfirmEmail")]
+        public async Task<IActionResult> ConfirmEmail(string token, string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user != null)
+            {
+                var result = await _userManager.ConfirmEmailAsync(user, token);
+                if (result.Succeeded)
+                {
+                    return StatusCode(StatusCodes.Status200OK,
+                      new Response { Status = "Success", Message = "Email Verified Successfully" });
+                }
+            }
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                       new Response { Status = "Error", Message = "This User Doesnot exist!" });
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            var loginOtpResponse=await _user.GetOtpByLoginAsync(loginModel);
+            if (loginOtpResponse.Response!=null)
+            {
+                var user = loginOtpResponse.Response.User;
+                if (user.TwoFactorEnabled)
+                {
+                    var token = loginOtpResponse.Response.Token;
+                    var message = new Message(new string[] { user.Email! }, "OTP Confrimation", token);
+                    _emailService.SendEmail(message);
+
+                    return StatusCode(StatusCodes.Status200OK,
+                     new Response { IsSuccess= loginOtpResponse.IsSuccess, Status = "Success", Message = $"We have sent an OTP to your Email {user.Email}" });
+                }
+                if (user != null && await _userManager.CheckPasswordAsync(user, loginModel.Password))
+                {
+                    var serviceResponse = await _user.GetJwtTokenAsync(user);
+                    return Ok(serviceResponse);
+
+                }
+            }
+            return Unauthorized();
+
+        }
+
+        [HttpPost]
+        [Route("login-2FA")]
+        public async Task<IActionResult> LoginWithOTP(string code, string userName)
+        {
+            var jwt =await _user.LoginUserWithJWTokenAsync(code, userName);
+            if (jwt.IsSuccess)
+            {
+                return Ok(jwt);
+            }
+            return StatusCode(StatusCodes.Status404NotFound,
+                new Response { Status = "Success", Message = $"Invalid Code" });
+        }
+
+        [HttpPost]
+        [Route("Refresh-Token")]
+        public async Task<IActionResult> RefreshToken(LoginResponse tokens)
+        {
+            var jwt = await _user.RenewAccessTokenAsync(tokens);
+            if (jwt.IsSuccess)
+            {
+                return Ok(jwt);
+            }
+            return StatusCode(StatusCodes.Status404NotFound,
+                new Response { Status = "Success", Message = $"Invalid Code" });
+        }
+
+        //
+
+    }
+}
